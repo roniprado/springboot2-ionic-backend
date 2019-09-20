2997e437fb952d50606b7b0d91a82739677b012c
Roni Prado <roni.geronimo@gmail.com>
2019-09-20 01:56:40 -0300

    Endpoint /clientes/{id} disponivel

---
+++ src/main/java/com/roni/cursomc/services/ClienteService.java
@@ -0,0 +1,26 @@
+package com.roni.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.roni.cursomc.domain.Cliente;
+import com.roni.cursomc.repositories.ClienteRepository;
+import com.roni.cursomc.services.exceptions.ObjectNotFoundException;
+
+
+@Service
+public class ClienteService {
+	
+	@Autowired
+	private ClienteRepository repo;
+	
+	public Cliente buscar(Integer id) {
+		Optional<Cliente> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado! ID: " + id 
+					+ ", Tipo: " + Cliente.class.getName()));
+		
+	}
+
+}
