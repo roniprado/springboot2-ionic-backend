2eecb17ab91384ad1e14a4b8ea8dfd973695cc1a
Roni Prado <roni.geronimo@gmail.com>
2019-09-23 01:08:23 -0300

    Validacao sintatica com Bean Validation

--- src/main/java/com/roni/cursomc/services/CategoriaService.java
+++ src/main/java/com/roni/cursomc/services/CategoriaService.java
@@ -11,6 +11,7 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.roni.cursomc.domain.Categoria;
+import com.roni.cursomc.dto.CategoriaDTO;
 import com.roni.cursomc.repositories.CategoriaRepository;
 import com.roni.cursomc.services.exceptions.DataIntegrityException;
 import com.roni.cursomc.services.exceptions.ObjectNotFoundException;
@@ -56,7 +57,10 @@ public class CategoriaService {
 	public Page<Categoria> findPage(Integer page, Integer linesPerPage, String orderBy, String direction){
 		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
 		return repo.findAll(pageRequest);
-		
+	}
+	
+	public Categoria fromDTO(CategoriaDTO objDto) {
+		return new Categoria(objDto.getId(), objDto.getNome());
 	}
 
 }
