9bed568c339df9af09197a12ff7219e8adab6a4d
Roni Prado <roni.geronimo@gmail.com>
2019-10-17 01:16:10 -0300

    PUT, DELETE e GET para Cliente

--- src/main/java/com/roni/cursomc/services/ClienteService.java
+++ src/main/java/com/roni/cursomc/services/ClienteService.java
@@ -1,12 +1,19 @@
 package com.roni.cursomc.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.roni.cursomc.domain.Cliente;
+import com.roni.cursomc.dto.ClienteDTO;
 import com.roni.cursomc.repositories.ClienteRepository;
+import com.roni.cursomc.services.exceptions.DataIntegrityException;
 import com.roni.cursomc.services.exceptions.ObjectNotFoundException;
 
 
@@ -21,6 +28,40 @@ public class ClienteService {
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado! ID: " + id 
 					+ ", Tipo: " + Cliente.class.getName()));
 		
+	}
+	
+	public Cliente update(Cliente obj) {
+		Cliente newObj = find(obj.getId());
+		updateData(newObj, obj);
+		return repo.save(newObj);
+	}
+	
+	public void delete(Integer id) {
+		find(id);
+		try {
+			repo.deleteById(id);
+		}
+		catch (DataIntegrityViolationException e) {
+			throw new DataIntegrityException("Não é possível excluir porque há entidades relacionadas");
+		}
+	}
+	
+	public List<Cliente> findAll(){
+		return repo.findAll();
+	}
+	
+	public Page<Cliente> findPage(Integer page, Integer linesPerPage, String orderBy, String direction){
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		return repo.findAll(pageRequest);
+	}
+	
+	public Cliente fromDTO(ClienteDTO objDto) {
+		return new Cliente(objDto.getId(),objDto.getNome(),objDto.getEmail(),null,null);
+	}
+	
+	private void updateData(Cliente newObj, Cliente obj) {
+		newObj.setNome(obj.getNome());
+		newObj.setEmail(obj.getEmail());
 	}
 
 }
